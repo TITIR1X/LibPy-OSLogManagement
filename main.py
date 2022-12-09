c9131dab939222adcfcb8b17776478834b99bff2
Santiago Benavidez <115203597+TITIR1X@users.noreply.github.com>
2022-12-09 20:06:01 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from OSLogManagement import os_log_parameters, show_last_log
 
 
 if __name__ == '__main__':
-        listado = os_log_parameters('ipconfig', regex_function='search', regex_parameters='IPv4.+: (\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})', show_log=True)
+        listado = os_log_parameters('ipconfig', regex_function='search', regex_parameters='IPv4.+: (\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})', export_log=True)
         string = ''.join(listado) # lista a string
         print(listado)
         print(string)
